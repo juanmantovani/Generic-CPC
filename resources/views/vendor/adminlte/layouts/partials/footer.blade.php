eac2e8215ebde7f63448054fae7df06c671ef926
Juan Cruz Mantovani <juan.mantovanid@gmail.com>
2022-03-17 15:57:06 -0300

    Commit inicial

--- resources/views/vendor/adminlte/layouts/partials/footer.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -2,7 +2,6 @@
 <!-- Main Footer -->
 <footer class="main-footer">
 	 <div align="right">
-&copy; Copyright 2019 Damian Agustin Sacks - Contacto: <a href="mailto:sacksdamian@gmail.com">sacksdamian@gmail.com</a>.
 	 </div>
 
 </footer>
